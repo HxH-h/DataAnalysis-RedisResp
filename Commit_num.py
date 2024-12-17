b1d8399d70f5970c24b3e498e40091c842ef7dfb
hxh <3148182735@qq.com>
2024-12-17 17:21:44 +0800

    折线图可视化unstable分支每月提交数

--- Commit_num.py
+++ Commit_num.py
@@ -1,15 +1,19 @@
 # 历史提交数的统计
+from datetime import datetime
 import time
-
+from functools import reduce
 import requests
 from lxml import etree
 import json
-import pandas as pd
+
 
 BRANCH_URL = 'https://github.com/redis/redis/branches/all?page='
 GITHUB_URL = 'https://github.com'
 COMMIT_URL = 'https://github.com/redis/redis/commits/'
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82'}
+HEADERS = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82',
+            "Authorization": f"Bearer "
+           }
 
 # 解析响应的HTML文件 将script下的json字符串转为字典
 # @Param resp 响应文本
@@ -17,6 +21,7 @@ def parseHTML(resp: str) -> dict:
     # xpath解析数据
     tree = etree.HTML(resp)
     script = tree.xpath('//turbo-frame[@id="repo-content-turbo-frame"]//script[@type="application/json"]')
+    json_data = {}
     try:
         json_data = json.loads(script[0].text)
     except Exception:
@@ -30,31 +35,92 @@ def getNum(num: str) -> int:
 # 获取仓库unstable分支的按月的提交数  返回字典
 # { year-month : num}  {2024-12:200 , 2024-11:100}
 def get_all_commit_num() -> dict:
-    ret = request_branch_commit('unstable')
-    # 提取月份
-    # 将字典转换为 DataFrame
-    df = pd.DataFrame(list(ret.items()), columns=['date_str', 'value'])
+    commit = request_branch_commit('unstable')
+    # 使用map和reduce统计按月的提交数
+    def map_data(item):
+        date_str = item['date']
+        date_obj = datetime.strptime(date_str, '%b %d, %Y')
+        year_month = date_obj.strftime('%Y-%m')
+        return (year_month, 1)
 
-    # 解析日期字符串为 datetime 对象
-    df['date'] = pd.to_datetime(df['date_str'], format='%b %d, %Y')
+    mapped_data = list(map(map_data, commit))
+    def reduce_data(acc , item):
+        year_month, count = item
+        if year_month not in acc:
+            acc[year_month] = 0
+        acc[year_month] += count
+        return acc
 
-    # 提取月份
-    df['month'] = pd.to_datetime(df['date'].dt.strftime('%b %Y'))
-
-    # 按月份分组并对值求和
-    summed_df = df.groupby('month')['value'].sum().reset_index().sort_values(by='month')
-
-    return dict(zip(summed_df['month'].dt.strftime('%b %Y'), summed_df['value']))
+    ret = reduce(reduce_data , mapped_data , {})
+    return ret
 
 # 获取所有的提交数 最后一次提交时间
-# 返回Branch列表
+# 返回字典列表
 def get_branch_commit_num() -> list:
+    branch = request_branch_commit_num()
+    def map_data(item):
+        ret = {
+            'name': item['name'],
+            'num': item['commit_num'],
+            'authoredDate': datetime.strptime(item['authoredDate'], '%Y-%m-%dT%H:%M:%S.%f%z').strftime('%Y-%m-%d')
+        }
+        return ret
+    return list(map(map_data, branch))
+
+
+
+# 发起请求获取一个分支下的commit数据
+# @Param branch 分支名
+# @return 每天的提交数
+def request_branch_commit(branch: str) -> list:
+    ret = []
+    base_num = 34
+    # 发起第一页的请求
+    resp = requests.get(url=COMMIT_URL + branch, headers=HEADERS , verify=False)
+
+    # 转为字典
+    json_data = parseHTML(resp.text)
+    datas = json_data['payload']['commitGroups']
+    # 获取每天的提交记录
+    for data in datas:
+        for commit in data['commits']:
+            ret.append({
+                'date': data['title'],
+                'authors': [ author['login'] for author in commit['authors']]
+            })
+    # 解析出最近一次提交的oid
+    base_oid = datas[0]['commits'][0]['oid']
+    # 设置分页大小
+    pagination = 35
+    # 其后的请求都基于base_oid + base_num + pagination
+    i = 0
+    while True:
+        url = COMMIT_URL + branch + '/?after=' + base_oid + '+' +str(base_num + pagination * i)
+        resp = requests.get(url=url, headers=HEADERS , verify=False)
+        json_data = parseHTML(resp.text)
+        datas = json_data['payload']['commitGroups']
+        # 获取每天的提交数
+        for data in datas:
+            for commit in data['commits']:
+                ret.append({
+                    'date': data['title'],
+                    'authors': [author['login'] for author in commit['authors']]
+                })
+
+        if i >= 5 or not json_data['payload']['filters']['pagination']['hasNextPage']:
+            break
+        print(base_num + pagination * i)
+        i += 1
+        time.sleep(1)
+    return ret
+
+def request_branch_commit_num() -> list:
     ret = []
     keys = ['name', 'path', 'authoredDate']
     # 获取所有分支名
     page = 1
     while True:
-        resp = requests.get(BRANCH_URL + str(page), headers=HEADERS , verify=False)
+        resp = requests.get(BRANCH_URL + str(page), headers=HEADERS, verify=False)
         # 解析出对象
         json_data = parseHTML(resp.text)
         # 获取分支名 和 最近一次提交时间
@@ -72,55 +138,13 @@ def get_branch_commit_num() -> list:
     # 获取每个分支的提交数
     for i in range(len(ret)):
         branch_url = ret[i]['path']
-        resp = requests.get(GITHUB_URL + branch_url, headers=HEADERS , verify=False)
+        resp = requests.get(GITHUB_URL + branch_url, headers=HEADERS, verify=False)
         json_data = parseHTML(resp.text)
         # 获取提交数
         ret[i]['commit_num'] = getNum(json_data['props']['initialPayload']['overview']['commitCount'])
         time.sleep(0.5)
 
     return ret
-
-
-
-# 发起请求获取一个分支下的commit数据
-# @Param branch 分支名
-# @return 每天的提交数
-def request_branch_commit(branch: str) -> dict:
-    ret = {}
-    base_num = 34
-    # 发起第一页的请求
-    resp = requests.get(url=COMMIT_URL + branch, headers=HEADERS , verify=False)
-
-    # 转为字典
-    json_data = parseHTML(resp.text)
-    datas = json_data['payload']['commitGroups']
-    # 获取每天的提交数
-    for data in datas:
-        ret[data['title']] = len(data['commits'])
-    # 解析出最近一次提交的oid
-    base_oid = datas[0]['commits'][0]['oid']
-    # 设置分页大小
-    pagination = 35
-    # 其后的请求都基于base_oid + base_num + pagination
-    i = 0
-    while True:
-        url = COMMIT_URL + branch + '/?after=' + base_oid + '+' +str(base_num + pagination * i)
-        resp = requests.get(url=url, headers=HEADERS , verify=False)
-        json_data = parseHTML(resp.text)
-        datas = json_data['payload']['commitGroups']
-        # 获取每天的提交数
-        for data in datas:
-            ret[data['title']] = len(data['commits'])
-
-        if not json_data['payload']['filters']['pagination']['hasNextPage']:
-            break
-        print(base_num + pagination * i)
-        i += 1
-        time.sleep(1)
-    return ret
-
-if __name__ == '__main__':
-    print(get_all_commit_num())
 
 
 
--- Visualization.py
+++ Visualization.py
@@ -1,1 +1,60 @@
 #  数据可视化
+import webbrowser
+import pyecharts.options as opts
+from pyecharts.charts import Line , Page
+from Commit_num import *
+
+#获取unstable分支下的每月的提交数
+unstable_commit_num = get_all_commit_num()
+
+# 绘制折线图
+ustable_line =(
+    Line(init_opts=opts.InitOpts(width="1500px"))
+        .add_xaxis(list(unstable_commit_num.keys()))
+        .add_yaxis("unstable",
+                   list(unstable_commit_num.values()),
+                   markpoint_opts=opts.MarkPointOpts(data=[opts.MarkPointItem(type_="max")])
+                  )
+        .set_global_opts(
+            legend_opts=opts.LegendOpts(is_show=False),
+            xaxis_opts=opts.AxisOpts(
+                axislabel_opts={"interval":"1" , "rotate":90}
+            ),
+            title_opts=opts.TitleOpts(title="unstable分支每月的提交数",
+                                      title_textstyle_opts=opts.TextStyleOpts(font_family='Kaiti'),
+                                      pos_left="center",
+                                      )
+        )
+)
+
+branch = get_branch_commit_num()
+
+
+
+
+#同时渲染多个图表
+(
+    Page(layout=Page.SimplePageLayout)
+        .add(
+            ustable_line
+        )
+        .render("visualization.html")
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 自动打开渲染好的html文件
+webbrowser.open("visualization.html")
